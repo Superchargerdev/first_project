13a847c7aea5ba2bb1c3a5d5dc8436f9c7d7cdb3
Superchargerdev <zychewiczj@gmail.com>
2023-07-11 14:17:10 +0200

    11.07.23

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,30 +3,39 @@
 namespace App\Controller;
 
 use App\Repository\MovieRepository;
-use App\Entity\Movie;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class FirstController extends AbstractController
 {
     private $em;
-    public function __construct(EntityManagerInterface $em)
+    private $movieRepository;
+    public function __construct(EntityManagerInterface $em, MovieRepository $movieRepository) 
     {
-        $this -> em = $em;
+        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
+
     #[Route('/first', name: 'app_first')]
     public function index(): Response
     {
-        //findAll() - Select * FROM movies;
-        //find() - Select * FROM movies WHERE id = 5;
-        //findBy() - Select * FROM movies ORDER BY id DESC;
-        //findOneBy - Select * FROM movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC;
-        //count() - Select * FROM movies WHERE id = 5;
-        // $repository = $this -> em -> getRepository(Movie::class)->findAll();
-        // $movies = $repository;
-        // dd($movies);
-        return $this->render('index.html.twig');
+        $movies = $this->movieRepository->findAll();
+
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
     }
+    
+    #[Route('/movies/{id}', methods: ['GET'], name: 'show_movie')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
+    }
+
 }
